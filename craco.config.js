67ea7378cb82028b854dd383b7d3c8cb26fc63be
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-02 00:12:32 +0300

    initial craco

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  reactScriptsVersion: "react-scripts",
+  webpack: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+};
