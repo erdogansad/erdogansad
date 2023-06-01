a70c93363890bb312dc345bec5db07e8a6105720
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-02 00:12:40 +0300

    custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purpleHearth: "#4731d3",
+        mineShaft: "#3a3a3a",
+        alabaster: "#f9f9f9",
+        jade: "#00ab6b",
+        deepCerulean: "#0077b5",
+        shiraz: "#af0c48",
+      },
+    },
   },
   plugins: [],
 };
