8d003dfc6b662343fa756b1d6f0f7e0a41d13552
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-02 16:15:35 +0300

    footer component created

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,31 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex flex-col justify-center items-center py-20">
+      <span className="text-4xl font-bold text-purpleHearth">Send me a message!</span>
+      <span className="text-center text-xl py-4">
+        Got a question or proposal, or just want
+        <br />
+        to say hello? Go ahead.
+      </span>
+      <span className="text-purpleHearth text-lg underline underline-offset-4 font-semibold">erdogan.sad@mail.com</span>
+      <div className="flex gap-x-4 text-purpleHearth text-3xl py-4">
+        <span>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={faGithub} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={faLinkedin} />
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
