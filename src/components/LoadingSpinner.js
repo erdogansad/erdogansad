a9c4bde996e406e340938a7416a8bbc98c063c2e
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-04 09:17:16 +0300

    loading component created

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const LoadingSpinner = () => {
-  return <div>LoadingSpinner</div>;
+  const { theme } = useSelector((store) => store.core);
+
+  return (
+    <div className={theme === "light" ? "light" : "dark"}>
+      <div className="flex justify-center items-center h-screen w-full bg-purpleHearth dark:bg-bunting">
+        <div className="absolute h-24 w-24 rounded-full border-8 border-solid border-trueV "></div>
+        <div
+          className="inline-block h-24 w-24  animate-spin rounded-full border-8 border-solid border-sulu  border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
