993dc498feb8dbb72c913846ad277c55615f22f1
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-04 09:17:45 +0300

    not found component created

--- src/pages/errors/NotFound.js
+++ src/pages/errors/NotFound.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const { notFound } = useSelector((store) => store.core.contents);
+
+  if (notFound) {
+    return (
+      <div className="h-screen w-screen bg-purpleHearth dark:bg-bunting flex flex-col gap-y-5 items-center justify-center">
+        <div className="flex flex-col justify-center items-center">
+          <span className="text-sulu font-bold text-5xl pb-5">{notFound[0]}</span>
+          <span className="text-bilobaFlower text-2xl pb-3">{notFound[1]}</span>
+          <span className="text-malibu text-lg">{notFound[2]}</span>
+        </div>
+        <Link to="/" className="py-3 px-5 rounded-xl font-bold bg-white dark:bg-balticSea text-purpleHearth dark:text-white">
+          {notFound[3]}
+        </Link>
+      </div>
+    );
+  }
 };
 
 export default NotFound;
