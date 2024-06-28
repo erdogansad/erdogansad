6c7caad2ff03658ae9b7ca0178bf428b4b802dda
erdogansad <erdogan.sad@windowslive.com>
2024-06-28 22:09:51 +0300

    refactor: Update ToTopButton component to use Button component for scroll-to-top functionality

--- src/@core/components/ToTopButton.js
+++ src/@core/components/ToTopButton.js
@@ -2,19 +2,15 @@ import { useWindowScroll } from "@uidotdev/usehooks";
 import React from "react";
 import { FaChevronUp } from "react-icons/fa";
 import ScrollTo from "react-scroll-into-view";
+import Button from "@components/Button";
 
 const ToTopButton = () => {
   const [{ y }] = useWindowScroll();
 
   return (
     <div className={`${y > 500 ? "bottom-4 opacity-100" : "-bottom-20 opacity-0"} transition-all duration-500 fixed end-4 group w-10 h-10 lg:h-12 lg:w-12`}>
-      <div className="absolute inset-0 transition-transform bg-blue-800 duration-200 translate-x-0 translate-y-0 group-hover:translate-x-1 group-hover:translate-y-1 rounded-xl w-full h-full -z-10"></div>
-      <ScrollTo
-        className={`p-2 rounded-xl group bg-blue-200 text-blue-900 border border-blue-800 cursor-pointer`}
-        scrollOptions={{ behavior: "smooth", block: "center", inline: "nearest" }}
-        selector={`#main`}
-      >
-        <FaChevronUp className="w-full h-full" />
+      <ScrollTo scrollOptions={{ behavior: "smooth", block: "center", inline: "nearest" }} selector={`#main`}>
+        <Button theme="third" ButtonIcon={FaChevronUp} />
       </ScrollTo>
     </div>
   );
