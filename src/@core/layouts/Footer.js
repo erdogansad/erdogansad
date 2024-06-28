62f41e36adb8561aae67849e70f8a860daae9040
erdogansad <erdogan.sad@windowslive.com>
2024-06-28 22:09:55 +0300

    refactor: Update Footer component to use Button component for social links

--- src/@core/layouts/Footer.js
+++ src/@core/layouts/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FiGithub } from "react-icons/fi";
 import { FaLinkedinIn, FaEnvelope } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import Button from "@components/Button";
 
 const socials = {
-  Github: <FiGithub className="w-full h-full" />,
-  Linkedin: <FaLinkedinIn className="w-full h-full" />,
-  "E-Mail": <FaEnvelope className="w-full h-full" />,
+  Github: FiGithub,
+  Linkedin: FaLinkedinIn,
+  "E-Mail": FaEnvelope,
 };
 
 const Footer = () => {
@@ -22,10 +22,7 @@ const Footer = () => {
         </div>
         <div className="flex items-center gap-2 lg:gap-5">
           {data.socials.map((social, index) => (
-            <Link key={index} to={social.url} target="_blank" rel="noopener noreferrer" className="relative w-10 h-10 lg:w-12 lg:h-12 group">
-              <div className="absolute inset-0 w-full h-full bg-blue-900 rounded-xl transition-transform duration-300 translate-x-0 translate-y-0 group-hover:translate-x-1 group-hover:translate-y-1"></div>
-              <span className="block w-full h-full rounded-xl bg-blue-50 border border-blue-900 p-2 text-blue-950 relative">{socials[social.title]}</span>
-            </Link>
+            <Button theme="secondary" key={index} to={social.url} ButtonIcon={socials[social.title]} />
           ))}
         </div>
         <span className="font-arimo lg:text-lg font-thin">{data.other_texts[2]}</span>
