601a8ff22aaef6993310e0a19cb2c4e2154c131e
erdogansad <erdogan.sad@windowslive.com>
2024-06-28 20:19:49 +0300

    refactor: Update ToTopButton component to fix positioning issue

--- src/@core/components/ToTopButton.js
+++ src/@core/components/ToTopButton.js
@@ -7,7 +7,7 @@ const ToTopButton = () => {
   const [{ y }] = useWindowScroll();
 
   return (
-    <div className={`${y > 500 ? "bottom-4 opacity-100" : "-bottom-20 opacity-0"} transition-all duration-500 fixed end-6 group w-10 h-10 lg:h-12 lg:w-12`}>
+    <div className={`${y > 500 ? "bottom-4 opacity-100" : "-bottom-20 opacity-0"} transition-all duration-500 fixed end-4 group w-10 h-10 lg:h-12 lg:w-12`}>
       <div className="absolute inset-0 transition-transform bg-blue-800 duration-200 translate-x-0 translate-y-0 group-hover:translate-x-1 group-hover:translate-y-1 rounded-xl w-full h-full -z-10"></div>
       <ScrollTo
         className={`p-2 rounded-xl group bg-blue-200 text-blue-900 border border-blue-800 cursor-pointer`}
