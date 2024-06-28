3a611df887d3699d4991b90b04d86f918a938ea0
erdogansad <erdogan.sad@windowslive.com>
2024-06-28 20:19:58 +0300

    refactor: Update Footer component to improve accessibility and styling

--- src/@pages/view/Home.js
+++ src/@pages/view/Home.js
@@ -3,7 +3,7 @@ import background from "@assets/img/background.png";
 import { Link } from "react-router-dom";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import ReactTypingEffect from "react-typing-effect";
-import { Transition } from "@headlessui/react";
+import { Transition, TransitionChild } from "@headlessui/react";
 import * as Icons from "devicons-react";
 import { TbWorldWww } from "react-icons/tb";
 import { useWindowScroll } from "@uidotdev/usehooks";
@@ -43,8 +43,9 @@ const Home = () => {
       <section id="main" className="h-screen">
         <div className="relative h-full w-full flex justify-center items-center">
           <div className="absolute h-full w-full bg-slate-900 -z-20"></div>
-          <Transition className="absolute h-full w-full flex justify-center items-center" show={sections.one}>
-            <Transition.Child
+          <Transition as="div" className="absolute h-full w-full flex justify-center items-center" show={sections.one}>
+            <TransitionChild
+              as="div"
               className="absolute top-0 h-full w-full bg-black -z-10"
               enter="transition ease duration-1000 transform"
               enterFrom="opacity-0 translate-y-12"
@@ -54,10 +55,10 @@ const Home = () => {
               leaveTo="opacity-0 translate-y-12"
             >
               <img className="h-full w-full object-cover object-bottom opacity-25" src={background} alt="" />
-            </Transition.Child>
+            </TransitionChild>
             <div className="flex flex-col items-center gap-5 lg:gap-10">
               <div className="flex flex-col items-center gap-2 lg:gap-8">
-                <Transition.Child
+                <TransitionChild
                   enter="transition ease duration-1000 transform delay-500"
                   enterFrom="opacity-0 translate-y-12"
                   enterTo="opacity-100 translate-y-0"
@@ -66,8 +67,8 @@ const Home = () => {
                   leaveTo="opacity-0 translate-y-12"
                 >
                   <span className="text-blue-50 text-3xl md:text-5xl xl:text-7xl font-bold cursor-default font-oswald">Erdoğan Şad</span>
-                </Transition.Child>
-                <Transition.Child
+                </TransitionChild>
+                <TransitionChild
                   enter="transition ease duration-1000 transform delay-1000"
                   enterFrom="opacity-0 translate-y-12"
                   enterTo="opacity-100 translate-y-0"
@@ -75,19 +76,19 @@ const Home = () => {
                   leaveFrom="opacity-100 translate-y-0"
                   leaveTo="opacity-0 translate-y-12"
                 >
-                  <span className="text-blue-50 md:text-xl xl:text-2xl font-semibold italic cursor-default font-arimo">
+                  <span className="text-blue-50 md:text-xl xl:text-2xl font-semibold italic cursor-default font-arimo select-none">
                     <ReactTypingEffect
                       text={shuffledList}
                       staticText={data.langs[0].code === "tr" ? "I am a" : "Ben bir"}
-                      speed="100"
-                      eraseSpeed="200"
+                      speed="50"
+                      eraseSpeed="100"
                       typingDelay="500"
                       eraseDelay="2500"
                     />
                   </span>
-                </Transition.Child>
+                </TransitionChild>
               </div>
-              <Transition.Child
+              <TransitionChild
                 enter="transition ease duration-1000 transform delay-[2000ms]"
                 enterFrom="opacity-0 translate-y-12"
                 enterTo="opacity-100 translate-y-0"
@@ -99,13 +100,7 @@ const Home = () => {
                   {data.socials.map(
                     (social, index) =>
                       social.title !== "E-Mail" && (
-                        <Link
-                          key={index}
-                          to={{ pathname: social.url }}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="relative px-3 py-1 lg:px-6 lg:py-2 group"
-                        >
+                        <Link key={index} to={social.url} target="_blank" rel="noopener noreferrer" className="relative px-3 py-1 lg:px-6 lg:py-2 group">
                           <span className="absolute inset-0 w-full h-full rounded-lg transition duration-300 ease-out transform translate-x-0 translate-y-0 bg-blue-50 group-hover:translate-x-1 group-hover:translate-y-1"></span>
                           <span className="absolute inset-0 w-full h-full bg-blue-900 border border-blue-50 rounded-lg"></span>
                           <div className="flex gap-1 relative items-center text-blue-50">
@@ -123,11 +118,12 @@ const Home = () => {
                       )
                   )}
                 </div>
-              </Transition.Child>
+              </TransitionChild>
             </div>
           </Transition>
         </div>
       </section>
+
       <section id={data.page_titles[0].el_id} className="bg-blue-200 py-12 lg:py-44">
         <div className="container flex flex-col lg:flex-row gap-12 lg:gap-32">
           <div
@@ -135,10 +131,10 @@ const Home = () => {
               sections.two ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
             } transition-all duration-1000 basis-1/2 space-y-4 lg:space-y-10`}
           >
-            <span className="text-4xl lg:text-6xl font-bold cursor-default font-oswald text-blue-950">{data.page_titles[0].title}</span>
+            <span className="text-4xl lg:text-6xl font-bold cursor-default font-oswald text-blue-950 select-none">{data.page_titles[0].title}</span>
             <div className="space-y-5">
               {data.about.map((about, index) => (
-                <p key={index} className="text-justify text-blue-900 font-arimo text-sm lg:text-base leading-7 cursor-default">
+                <p key={index} className="text-justify text-blue-900 font-arimo text-sm lg:text-base leading-7 cursor-default select-none">
                   {about}
                 </p>
               ))}
@@ -149,27 +145,32 @@ const Home = () => {
               sections.two ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
             } transition-all duration-1000 delay-200 basis-1/2 space-y-4 lg:space-y-10`}
           >
-            <span className="text-4xl lg:text-6xl font-bold cursor-default font-oswald text-blue-950">{data.page_titles[1].title}</span>
+            <span className="text-4xl lg:text-6xl font-bold cursor-default font-oswald text-blue-950 select-none">{data.page_titles[1].title}</span>
             <div className="flex flex-wrap gap-3 lg:gap-6 justify-center">
               {data.skills.map((skill, index) => {
                 const Skill = Icons[skill.icon];
-                return (
-                  <span key={index} className="w-14 h-14 lg:w-20 lg:h-20">
-                    <Skill size="full" />
-                  </span>
-                );
+                if (Skill) {
+                  return (
+                    <span key={index} className="w-14 h-14 lg:w-20 lg:h-20">
+                      <Skill size="full" />
+                    </span>
+                  );
+                } else {
+                  return skill.icon;
+                }
               })}
             </div>
           </div>
         </div>
       </section>
+
       <section id={data.page_titles[2].el_id} className="bg-blue-950 py-12 lg:py-44">
         <div
           className={`${
             sections.three ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
           } transition-all duration-1000 container space-y-4 lg:space-y-12`}
         >
-          <span className="text-4xl lg:text-6xl font-bold cursor-default font-oswald text-blue-200">{data.page_titles[2].title}</span>
+          <span className="text-4xl lg:text-6xl font-bold cursor-default font-oswald select-none text-blue-200">{data.page_titles[2].title}</span>
           <div className="grid lg:grid-cols-3 gap-8">
             {data.projects.map((project, index) => {
               const projectData = JSON.parse(project.data);
@@ -181,17 +182,17 @@ const Home = () => {
                   } group`}
                 >
                   <div className="absolute inset-0 h-full w-full bg-blue-500 -z-10 rounded-xl transition-transform duration-500 translate-x-0 translate-y-0 group-hover:translate-x-2 group-hover:translate-y-2"></div>
-                  <img src={project.image} className="transition-all duration-400 w-full h-full object-cover rounded-xl" alt="" />
+                  <img src={project.image} className="transition-all duration-400 w-full h-full object-cover rounded-xl group-hover:blur" alt="" />
                   <div className="transition-all delay-[100ms] duration-400 absolute inset-0 flex translate-y-2 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 flex-col h-full py-3 px-5 rounded-xl gap-1 lg:gap-3 bg-blue-200/75">
-                    <span className="transition-transform duration-500 delay-100 -translate-y-4 opacity-0 lg:text-2xl font-bold font-oswald text-blue-950 block text-center cursor-default group-hover:opacity-100 group-hover:translate-y-0">
+                    <span className="transition-transform duration-500 delay-100 -translate-y-4 opacity-0 lg:text-2xl font-bold font-oswald text-blue-950 block text-center cursor-default select-none group-hover:opacity-100 group-hover:translate-y-0">
                       {projectData.title}
                     </span>
-                    <p className="hidden lg:block transition-transform duration-500 delay-100 translate-y-4 opacity-0 text-blue-950 font-arimo text-sm text-justify grow cursor-default group-hover:opacity-100 group-hover:translate-y-0">
+                    <p className="hidden lg:block transition-transform duration-500 delay-100 translate-y-4 opacity-0 text-blue-950 font-arimo text-sm text-justify grow cursor-default select-none group-hover:opacity-100 group-hover:translate-y-0">
                       {projectData.description}
                     </p>
                     <div className="flex justify-between items-center text-blue-950 grow shrink-0 lg:grow-0 lg:shrink">
                       <Link
-                        to={{ pathname: project.github }}
+                        to={project.github}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="transition-all duration-500 delay-100 group/inner -translate-x-6 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 w-10 h-10"
@@ -202,7 +203,7 @@ const Home = () => {
                         </div>
                       </Link>
                       <Link
-                        to={{ pathname: project.live }}
+                        to={project.live}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="transition-all duration-500 delay-100 group/inner translate-x-6 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 w-10 h-10"
