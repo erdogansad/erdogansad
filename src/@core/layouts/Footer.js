7e6f72c26bfb266c5971f1e8d62e7b74eb36b330
erdogansad <erdogan.sad@windowslive.com>
2024-06-28 20:19:54 +0300

    refactor: Update Footer component to improve accessibility and styling

--- src/@core/layouts/Footer.js
+++ src/@core/layouts/Footer.js
@@ -17,12 +17,12 @@ const Footer = () => {
     <footer id="contact" className="w-full bg-blue-100">
       <div className="container mx-auto flex flex-col gap-6 items-center justify-center py-12 lg:py-16">
         <div className="flex flex-col items-center gap-3">
-          <span className="font-oswald text-xl lg:text-4xl font-bold cursor-default">{data.other_texts[0]}</span>
-          <span className="font-arimo text-xs lg:text-lg cursor-default">{data.other_texts[1]}</span>
+          <span className="font-oswald text-xl text-blue-950 lg:text-4xl font-bold select-none cursor-default">{data.other_texts[0]}</span>
+          <span className="font-arimo text-xs lg:text-lg select-none cursor-default">{data.other_texts[1]}</span>
         </div>
         <div className="flex items-center gap-2 lg:gap-5">
           {data.socials.map((social, index) => (
-            <Link key={index} to={{ pathname: social.url }} target="_blank" rel="noopener noreferrer" className="relative w-10 h-10 lg:w-12 lg:h-12 group">
+            <Link key={index} to={social.url} target="_blank" rel="noopener noreferrer" className="relative w-10 h-10 lg:w-12 lg:h-12 group">
               <div className="absolute inset-0 w-full h-full bg-blue-900 rounded-xl transition-transform duration-300 translate-x-0 translate-y-0 group-hover:translate-x-1 group-hover:translate-y-1"></div>
               <span className="block w-full h-full rounded-xl bg-blue-50 border border-blue-900 p-2 text-blue-950 relative">{socials[social.title]}</span>
             </Link>
