9eadcc513409f4156131af399a510116b681de08
erdogansad <erdogan.sad@windowslive.com>
2024-09-12 00:39:51 +0300

    refactor: Update craco.config.js and package.json

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,16 @@
 const path = require("path");
+const CracoEnvPlugin = require("craco-plugin-env");
 
 module.exports = {
   reactScriptsVersion: "react-scripts",
+  plugins: [
+    {
+      plugin: CracoEnvPlugin,
+      options: {
+        variables: {},
+      },
+    },
+  ],
   webpack: {
     configure: {
       resolve: {
