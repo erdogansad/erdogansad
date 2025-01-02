365638d580fd4291d05678566ca7fd69b0d32a60
erdogansad <erdogan.sad@windowslive.com>
2025-01-02 23:51:57 +0300

    refactor: Ensure LoadingSpinner returns JSX in fullpage mode

--- src/core/components/LoadingSpinner.tsx
+++ src/core/components/LoadingSpinner.tsx
@@ -1,13 +1,15 @@
-const LoadingSpinner = ({ size, fullpage }: { size?: number; fullpage?: boolean }) => {
+const LoadingSpinner = ({ size, fullpage }: { size: number; fullpage?: boolean }) => {
   if (fullpage) {
-    <div className="h-screen flex justify-center items-center bg-slate-900">
-      <div
-        style={size ? { height: `${size / 4}rem`, width: `${size / 4}rem` } : {}}
-        className={`${size !== undefined ? "" : "h-16 w-16"} inline-block animate-ping rounded-full bg-blue-500`}
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+    return (
+      <div className="h-screen flex justify-center items-center bg-slate-900">
+        <div
+          style={size ? { height: `${size / 4}rem`, width: `${size / 4}rem` } : {}}
+          className={`${size !== undefined ? "" : "h-16 w-16"} inline-block animate-ping rounded-full bg-blue-500`}
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+        </div>
       </div>
-    </div>;
+    );
   } else {
     return (
       <div
