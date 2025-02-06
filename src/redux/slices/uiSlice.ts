b0b46e268f5bb1eda3c0d5bf11cb143a24e8b111
erdogansad <erdogan.sad@windowslive.com>
2025-02-06 12:13:10 +0300

    refactor: Remove unused fetchDarkMode function from uiSlice for cleaner code

--- src/redux/slices/uiSlice.ts
+++ src/redux/slices/uiSlice.ts
@@ -24,37 +24,12 @@ const uiSlice = createSlice({
       document.body.classList.toggle("dark");
       document.body.classList.toggle("bg-slate-950");
     },
-    fetchDarkMode: (state) => {
-      const darkMode = localStorage.getItem("darkMode");
-      if (darkMode !== undefined && darkMode !== null) {
-        state.darkMode = darkMode === "true";
-        if (state.darkMode) {
-          document.body.classList.add("dark");
-          document.body.classList.add("bg-slate-950");
-        } else {
-          document.body.classList.remove("dark");
-          document.body.classList.remove("bg-slate-950");
-        }
-      } else {
-        if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-          state.darkMode = true;
-          document.body.classList.add("dark");
-          document.body.classList.add("bg-slate-950");
-          localStorage.setItem("darkMode", "true");
-        } else {
-          state.darkMode = false;
-          document.body.classList.remove("dark");
-          document.body.classList.remove("bg-slate-950");
-          localStorage.setItem("darkMode", "false");
-        }
-      }
-    },
     setLoading(state, action) {
       state.loading = action.payload;
     },
   },
 });
 
-export const { setDarkMode, toggleDarkMode, fetchDarkMode, setLoading } = uiSlice.actions;
+export const { setDarkMode, toggleDarkMode, setLoading } = uiSlice.actions;
 export default uiSlice.reducer;
 export type { UIState };
